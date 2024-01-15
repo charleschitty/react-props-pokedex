bc165ddce70f951dbd6d5a6b497e2b10e42b1580
Charles Chitty <charlescchitty@gmail.com>
2024-01-15 15:36:30 -0800

    Added skeletons for Pokecard and Pokedex files

---
+++ src/Pokecard.js
@@ -0,0 +1,16 @@
+
+
+const POKEMON_IMAGE_SOURCE = `htpps://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+/** Shows a single pokemon, with their name, image, and type */
+function Pokecard() {
+  return (
+    <div>
+      name:
+      image:
+      type:
+    </div>
+  )
+};
+
+
+export { Pokecard }
---
+++ src/Pokedex.js
@@ -0,0 +1,14 @@
+import Pokecard from "./Pokecard"
+
+
+/** Given an array of objects describing different pokemon,
+ *  renders a sequence of Pokecard components
+ *
+ *  Ex Input: [{id: 4, name: 'Charmander', type: 'fire', base_experience: 62}]
+ */
+function Pokedex(props){
+
+};
+
+
+export { Pokedex }
